815fdb2d81c743236daddc3a55eadb5ae52a269e
Natalia-Havashi <natali.gavashi@gmail.com>
2023-08-05 15:02:17 +0300

    Revies

--- src/components/Revies/Revies.jsx
+++ src/components/Revies/Revies.jsx
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getMovieReviews } from "servise/servise";
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { getMovieReviews } from "servise/servise";
 
-const Revies = () => {
-    const {movieId} = useParams();
-    const [revies, setRevies] = useState('');
-    useEffect(() => {
-        const getRevies = async movieId => {
-            try{
-                const {results} = await getMovieReviews(movieId)
-            setRevies(results) 
-            }catch(error) {
-                console.log(error)
-            }
+// const Revies = () => {
+//     const {movieId} = useParams();
+//     // const [revies, setRevies] = useState('');
+//     useEffect(() => {
+//         const getRevies = async movieId => {
+//             try{
+//                 const {results} = await getMovieReviews(movieId)
+//             setRevies(results) 
+//             }catch(error) {
+//                 console.log(error)
+//             }
            
-        }
-        getRevies(movieId)
-    }, [movieId])
-    return (
-        <div>
-            <h3>Cast</h3>
-            <ul>
-                <li></li>
-                <p>Інформація про огляд фільму</p>
-            </ul>
-        </div>
-   )
-}
+//         }
+//         getRevies(movieId)
+//     }, [movieId])
+//     return (
+//         <div>
+//             <h3>Cast</h3>
+//             <ul>
+//                 <li></li>
+//                 <p>Інформація про огляд фільму</p>
+//             </ul>
+//         </div>
+//    )
+// }
 
-export default Revies;
+// export default Revies;
